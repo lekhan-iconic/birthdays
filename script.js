2b6998407874c998a650608f889a152b26f6c6b1
Narra Lekhan Kumar <132564594+lekhan-iconic@users.noreply.github.com>
2024-12-18 23:01:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     hour = minute * 60,
     day = hour * 24;
 
-  let birthday = "Nov 18, 2024 00:00:00",
+  let birthday = "Nov 18, 2025 00:00:00",
     countDown = new Date(birthday).getTime(),
     x = setInterval(function () {
       let now = new Date().getTime(),
